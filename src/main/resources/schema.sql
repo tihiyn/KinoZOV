8c2f424e5dbdd26efd2c5458740c10fb1a9cbfd0
Vladimir Anisimov <anisimov11122003@gmail.com>
2024-03-16 17:05:19 +0300

    Fixed db connection bugs

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 -- users
 create table if not exists myuser
 (
-    id integer primary key,
+    id serial primary key,
     name varchar not null,
     password varchar not null,
     role varchar not null
